3d8fc2fdfeeeef394bbf903b39341d043b1c01cc
yonakahara <peter20000118@gmail.com>
2021-08-20 23:59:29 +0900

    add create event functionality

--- flaskr/calendar.py
+++ flaskr/calendar.py
@@ -1,5 +1,7 @@
 import functools
 import re
+import json
+import collections
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, request, jsonify, current_app
@@ -12,15 +14,31 @@ bp = Blueprint('calendar', __name__, url_prefix='/cal')
 
 @bp.route("/", methods=("GET", "POST"))
 def show_cal():
-    current_app.logger.debug(request.method)
+    db = get_db()
     if request.method == "POST": 
         current_app.logger.debug(request.form)
         date_start = request.form['date_start']
         date_end = request.form['date_end']
         event_title = request.form['event_title']
-        output = "event {} is from {} to {}. ".format(event_title, date_start, date_end)
-        current_app.logger.debug(output)
+
+        # store event to database
+        db.execute("INSERT INTO event (user_id, start, end, title) VALUES (?, ?, ?, ?)", 
+                   (1, date_start, date_end, event_title))
+        db.commit()
+
+        # ajax
         if date_start and date_end and event_title:
             return jsonify({'start':date_start, 'end':date_end, 'title':event_title})
         return jsonify({'error' : 'Missing data!'})
-    return render_template("calendar/cal.html")
+    
+    # GET: take out events and pass to calendar object
+    events_rows = db.execute("SELECT * FROM event WHERE user_id = 1").fetchall()
+    events_list = []
+    for row in events_rows: 
+        d = collections.OrderedDict()
+        d["start"] = row["start"]
+        d["end"] = row["end"]
+        d["title"] = row["title"]
+        # d["user_id"] = int(row["user_id"])
+        events_list.append(d)
+    return render_template("calendar/cal.html", events = events_list)
